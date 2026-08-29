7e4e5a7481d89748559e0fb6e83b5063a0a67c22
kgunnn20-gif <k.gunnn20@gmail.com>
2026-08-29 19:48:59 +0300

    Add files via upload

---
+++ kendimi anlatan uygulama/main.py
@@ -0,0 +1,35 @@
+# Import
+from flask import Flask, render_template,request, redirect
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dinamik beceriler
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/giris")
+def giris():
+    return render_template("index2.html")
+
+app.run(debug=True)
